5fb7b8432be0c3facc9462a16e5349040fb49be8
SirPhemmiey <oluwafemiakinde@gmail.com>
2022-07-10 17:41:36 +0400

    chore: add create, delete, update and validate scripts

---
+++ create.sh
@@ -0,0 +1,3 @@
+aws cloudformation create-stack --stack-name prometheus --template-body file://$1  --parameters file://$2 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+
+#./create.sh template.yaml parameters.json
